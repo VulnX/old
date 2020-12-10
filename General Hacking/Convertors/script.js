e998206de28d49981fdbecb623817ea2dd3635d3
VulnX <vulnx101@gmail.com>
2020-12-10 12:50:03 +0530

    Minor UI Chnages

--- General Hacking/Convertors/script.js
+++ General Hacking/Convertors/script.js
@@ -13,7 +13,7 @@ function ascii2hex() {
 	var str = document.getElementById('ASCII').value;
 	var arr1 = [];
 	for (var i = 0; i < str.length; i++) {
-		var ascii = "0x"
+		var ascii = "";
 		ascii = ascii.concat(Number(str.charCodeAt(i)).toString(16));
 		ascii = ascii.concat(' ');
 		arr1.push(ascii);
