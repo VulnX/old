d13a46fd8debc1ad08df7b661ed04878e0b79dc4
VulnX <vulnx101@gmail.com>
2020-12-12 01:14:35 +0530

    Minor UI Chnages

--- General Hacking/Convertors/script.js
+++ General Hacking/Convertors/script.js
@@ -23,3 +23,13 @@ function ascii2hex() {
 		document.getElementById('copy').style.display = "block";
 	}
 }
+
+function hex2ascii(str1)
+ {
+	var hex  = str1.toString();
+	var str = '';
+	for (var n = 0; n < hex.length; n += 3) {
+		str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+	}
+	return str;
+ }
